c403bfbcc756fa8e775ebf9049abfcf4a9dd0c38
Selvyn <swright@celestial.co.uk>
2022-12-03 00:18:09 +0000

    automation version

--- src/main/java/com/celestial/pingme/controllers/PingController.java
+++ src/main/java/com/celestial/pingme/controllers/PingController.java
@@ -16,6 +16,31 @@ import com.celestial.pingme.services.PingService;
 public class PingController 
 {
 	final private	String	PINGME_ENDPOINT = "PINGME_ENDPOINT"; 
+	/* Kubernetes will create a number of environment variables for you so you can located other PODs
+	 * If you a Service of type NodePort like this
+	 * apiVersion: v1
+	 * kind: Service
+	 * metadata:
+	 *	  name: remote-pingme-server-ep
+	 *	  namespace: default
+	 * spec:
+	 *	  type: NodePort
+	 *	  selector:
+	 *	    service_id: remote-pingme-server
+	 *	  ports:
+	 *	  - nodePort: 30082
+	 *	    port: 8080
+	 *	    targetPort: 8080
+	 *
+	 * from the metadata.name remote-pingme-server-ep, K8 will create two an environment variables
+	 * REMOTE_PINGME_SERVER_EP_SERVICE_HOST with the IP address of the POD's cluster
+	 * and
+	 * REMOTE_PINGME_SERVER_EP_SERVICE_PORT with the port that POD is exposed on on that cluster
+	 * 
+	 * With this information we can automate the linking between the PODs
+	 */
+	final private	String	PINGME_K8_ENDPOINT = "REMOTE_PINGME_SERVER_EP_SERVICE_HOST"; 
+	final private	String	PINGME_K8_PORT = "REMOTE_PINGME_SERVER_EP_SERVICE_PORT"; 
 	private final String TIME_API = "/api/time";
 	private	PingService thePingService;
 	
@@ -47,13 +72,24 @@ public class PingController
 	{
 		LocalTime time = LocalTime.now();
 		String theTime = "[NOT SET]";
-		String dbEnpoint = System.getenv().getOrDefault(PINGME_ENDPOINT, null);
-		
-		if(dbEnpoint != null )
+		String endpoint = System.getenv().getOrDefault(PINGME_ENDPOINT, null);
+
+		// Grads don't get this version, they must pass the environment variable
+		if( endpoint == null )
+		{
+			endpoint = System.getenv().getOrDefault(PINGME_K8_ENDPOINT, null);
+			if( endpoint != null )
+			{
+				String port = System.getenv().getOrDefault(PINGME_K8_PORT, "8080");
+				endpoint = "http://" + endpoint + ":" + port;
+			}
+		}
+				
+		if(endpoint != null )
 		{
 			theTime = webClientBuilder.build()
 						.get()
-						.uri(dbEnpoint + TIME_API)
+						.uri(endpoint + TIME_API)
 						.retrieve()
 						.bodyToMono(String.class)
 						.block();
